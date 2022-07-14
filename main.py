1946dcf87dcf4c1dfea3a223d72b667b9230a2f3
vera <lerika250293@mail.ru>
2022-07-14 12:58:53 +0300

    add

--- main.py
+++ main.py
@@ -26,16 +26,13 @@
 import random
 def cadrdesk():
     cadr_desk = ['2 треф', '2 бубны', '2 черви', '2 пики', '3 треф', '3 бубны', '3 черви', '3 пики', '4 треф', '4 бубны', '4 черви', '4 пики', '5 треф', '5 бубны', '5 черви', '5 пики', '6 треф', '6 бубны', '6 черви', '6 пики', '7 треф', '7 бубны', '7 черви', '7 пики', '8 треф', '8 бубны', '8 бубны', '8 черви', '8 пики', '9 треф', '9 бубны', '9 черви', '9 пики', '10 треф', '10 бубны', '10 черви', '10 пики', 'A треф', 'A бубны', 'A черви', 'A пики', 'J треф', 'J бубны', 'J черви', 'J пики', 'D треф', 'D бубны', 'D черви', 'D пики', 'K треф', 'K бубны', 'K черви', 'K пики']
-    if len(cadr_desk)!=0:
-        y = random.choice(cadr_desk)
-        cadr_desk.remove(y)
-        print(y)
+
+    for i in cadr_desk:
+        yield i
+        print(cadr_desk.remove(i))
+        print(cadr_desk)
         print(len(cadr_desk))
-        yield cadr_desk.pop()
 
-    else:
-        yield 'StopIteration'
-
-
-d = cadrdesk()
-print(d.__next__())
+y = cadrdesk()
+for i in y:
+    print(i)
